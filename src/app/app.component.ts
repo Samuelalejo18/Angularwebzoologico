1e234af07563096f4a14c6fee33ef8b360bb393c
samuel monsalve <samuelalejomonsalve@gmail.com>
2025-04-29 19:11:51 -0500

    get animals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AnimalComponent } from './components/animal/animal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, AnimalComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'webzoologico';
